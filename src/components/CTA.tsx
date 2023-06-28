6c8f322de4c1f1e763beb1f0ff51782e0098800b
Caleb McQuaid <calebmcquaid@gmail.com>
2023-06-28 01:42:21 -0400

    Pull out CTA into component

---
+++ src/components/CTA.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Text } from "@mantine/core";
+
+const CTA: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        background: "#FBFBFD",
+      }}
+    >
+      <Text size={24} fw={700} mt={120} mb={40}>
+        Ready to get started?
+      </Text>
+      <Text mb={25} ta="center" sx={{ maxWidth: "420px" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Text>
+      <Button
+        mb={120}
+        h={60}
+        w={200}
+        sx={{
+          borderRadius: 10,
+          fontSize: 18,
+          background: "#301F55",
+          padding: 20,
+        }}
+      >
+        Start free trial
+      </Button>
+    </Box>
+  );
+};
+
+export default CTA;
