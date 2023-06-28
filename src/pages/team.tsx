207434b2e0c6057a838de789b11736e8a3bd0739
matthew-woodington <smwoodington@gmail.com>
2023-06-28 16:53:09 -0400

    team page head

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -6,8 +6,22 @@ import { PageWithLayout } from "next";
 const TeamPage: PageWithLayout = () => {
   return (
     <AppLayout>
-      <Box>
-        <Text>Team Page</Text>
+      <Box sx={{ display: "flex", flexDirection: "column", color: "#000" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "400px",
+            background: "#EAD8AD",
+          }}
+        >
+          <Text size={48} fw={700} mb={25}>Meet Our Team</Text>
+          <Text ta="center" size={20} sx={{ maxWidth: 645}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Text>
+        </Box>
       </Box>
     </AppLayout>
   );
