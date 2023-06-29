3dd73a00d0652fa7f9d873055b9b76b75ff0f1de
matthew-woodington <smwoodington@gmail.com>
2023-06-28 21:31:23 -0400

    footer color change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,40 @@
 import { Box, Button, Divider, Text, Image } from "@mantine/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 export default function Footer() {
   const date = new Date();
   const year = date.getFullYear();
+  const [colors, setColors] = useState({
+    background: "",
+    text: "",
+    button: "",
+  })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.pathname.includes("/features")) {
+      setColors({
+        background: "#C9796F",
+        text: "#000",
+        button: "#301F55",
+      })
+    } else if (router.pathname.includes("/team")) {
+      setColors({
+        background: "#EAD8AD",
+        text: "#000",
+        button: "#301F55",
+      })
+    } else {
+      setColors({
+        background: "#252634",
+        text: "#FBFBFD",
+        button: "#8252C6",
+      })
+    }
+  }, [router.pathname])
 
   return (
     <Box
@@ -13,18 +44,20 @@ export default function Footer() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        background: "#252634",
-        color: "#FBFBFD"
+        // background: "#252634",
+        // color: "#FBFBFD"
+        background: colors.background,
+        color: colors.text,
       }}
     >
       <Box sx={{ display: "flex", flexDirection: "column", width: "70%" }}>
         <Box mb={40} sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", position: "relative" }}>
           <Box sx={{ display: "flex" }}>
             <Link href="#" style={{ textDecoration: "none", marginLeft: "50px" }}>
-              <Text size={14} fw={700} color={"#FBFBFD"}>Our Team</Text>
+              <Text size={14} fw={700} color={colors.text}>Our Team</Text>
             </Link>
             <Link href="#" style={{ textDecoration: "none", marginLeft: "50px" }}>
-              <Text size={14} fw={700} color={"#FBFBFD"}>Features</Text>
+              <Text size={14} fw={700} color={colors.text}>Features</Text>
             </Link>
           </Box>
           <Box sx={{ position: "absolute", right: "50%", transform: "translateX(50%)" }}>
@@ -38,15 +71,15 @@ export default function Footer() {
           </Box>
           <Box sx={{ display: "flex" }}>
             <Link href="#" style={{ textDecoration: "none", marginRight: "50px" }}>
-              <Text size={14} fw={700} color={"#FBFBFD"}>Plans</Text>
+              <Text size={14} fw={700} color={colors.text}>Plans</Text>
             </Link>
             <Link href="#" style={{ textDecoration: "none", marginRight: "50px" }}>
-              <Text size={14} fw={700} color={"#FBFBFD"}>Home</Text>
+              <Text size={14} fw={700} color={colors.text}>Home</Text>
             </Link>
           </Box>
         </Box>
-        <Divider size="sm" color="#FBFBFD" />
-        <Button mt={50} h={60} w={200} radius={10} sx={{ alignSelf: "center", background: "#8252C6", fontSize: 18, }}>Get in Touch</Button>
+        <Divider size="sm" color={colors.text} />
+        <Button mt={50} h={60} w={200} radius={10} sx={{ alignSelf: "center", background: colors.button, fontSize: 18, }}>Get in Touch</Button>
         <Box mt={50} sx={{ display: "flex", alignSelf: "center", gap: "20px" }}>
           <Text>©️ 2010-{year}</Text>
           <Text>Privacy-Terms</Text>
