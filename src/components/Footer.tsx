373dcab4f24e62e8c4bf87bfe1396162cc713d9a
Caleb McQuaid <calebmcquaid@gmail.com>
2023-07-02 01:55:39 -0400

    Mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, Text, Image } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
@@ -10,9 +11,10 @@ export default function Footer() {
     background: "",
     text: "",
     button: "",
-  })
+  });
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     if (router.pathname.includes("/features")) {
@@ -20,71 +22,199 @@ export default function Footer() {
         background: "#C9796F",
         text: "#000",
         button: "#301F55",
-      })
+      });
     } else if (router.pathname.includes("/team")) {
       setColors({
         background: "#EAD8AD",
         text: "#000",
         button: "#301F55",
-      })
+      });
     } else {
       setColors({
         background: "#252634",
         text: "#FBFBFD",
         button: "#8252C6",
-      })
+      });
     }
-  }, [router.pathname])
+  }, [router.pathname]);
 
   return (
-    <Box
-      h={410}
-      w={"100%"}
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        // background: "#252634",
-        // color: "#FBFBFD"
-        background: colors.background,
-        color: colors.text,
-      }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column", width: "70%" }}>
-        <Box mb={40} sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", position: "relative" }}>
-          <Box sx={{ display: "flex" }}>
-            <Link href="/team" style={{ textDecoration: "none", marginLeft: "50px" }}>
-              <Text size={14} fw={700} color={colors.text}>Our Team</Text>
+    <>
+      {isMobile ? (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: colors.background,
+            color: colors.text,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "70%",
+              alignItems: "center",
+            }}
+          >
+            <Link
+              href="/team"
+              style={{ textDecoration: "none", margin: "25px" }}
+            >
+              <Text size={14} fw={700} color={colors.text}>
+                Our Team
+              </Text>
             </Link>
-            <Link href="/features" style={{ textDecoration: "none", marginLeft: "50px" }}>
-              <Text size={14} fw={700} color={colors.text}>Features</Text>
+            <Link
+              href="/features"
+              style={{ textDecoration: "none", margin: "25px" }}
+            >
+              <Text size={14} fw={700} color={colors.text}>
+                Features
+              </Text>
             </Link>
-          </Box>
-          <Box sx={{ position: "absolute", right: "50%", transform: "translateX(50%)" }}>
-            <Link href="/" >
+            <Link
+              href="/plans"
+              style={{
+                textDecoration: "none",
+                margin: "25px",
+              }}
+            >
+              <Text size={14} fw={700} color={colors.text}>
+                Plans
+              </Text>
+            </Link>
+            <Link href="/" style={{ margin: "25px" }}>
               <Image
                 src="/images/logo-light-400w.png"
                 width={100}
                 alt="scripto logo"
               />
             </Link>
-          </Box>
-          <Box sx={{ display: "flex" }}>
-            <Link href="/plans" style={{ textDecoration: "none", marginRight: "50px" }}>
-              <Text size={14} fw={700} color={colors.text}>Plans</Text>
-            </Link>
-            <Link href="/" style={{ textDecoration: "none", marginRight: "50px" }}>
-              <Text size={14} fw={700} color={colors.text}>Home</Text>
-            </Link>
+            <Button
+              mt={25}
+              h={60}
+              w={200}
+              radius={10}
+              sx={{
+                alignSelf: "center",
+                background: colors.button,
+                fontSize: 18,
+              }}
+            >
+              Get in Touch
+            </Button>
+            <Box
+              mt={50}
+              mb={25}
+              sx={{ display: "flex", alignSelf: "center", gap: "20px" }}
+            >
+              <Text>©️ 2010-{year}</Text>
+              <Text>Privacy-Terms</Text>
+            </Box>
           </Box>
         </Box>
-        <Divider size="sm" color={colors.text} />
-        <Button mt={50} h={60} w={200} radius={10} sx={{ alignSelf: "center", background: colors.button, fontSize: 18, }}>Get in Touch</Button>
-        <Box mt={50} sx={{ display: "flex", alignSelf: "center", gap: "20px" }}>
-          <Text>©️ 2010-{year}</Text>
-          <Text>Privacy-Terms</Text>
+      ) : (
+        <Box
+          h={410}
+          w={"100%"}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            // background: "#252634",
+            // color: "#FBFBFD"
+            background: colors.background,
+            color: colors.text,
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column", width: "70%" }}>
+            <Box
+              mb={40}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                position: "relative",
+              }}
+            >
+              <Box sx={{ display: "flex" }}>
+                <Link
+                  href="/team"
+                  style={{ textDecoration: "none", marginLeft: "50px" }}
+                >
+                  <Text size={14} fw={700} color={colors.text}>
+                    Our Team
+                  </Text>
+                </Link>
+                <Link
+                  href="/features"
+                  style={{ textDecoration: "none", marginLeft: "50px" }}
+                >
+                  <Text size={14} fw={700} color={colors.text}>
+                    Features
+                  </Text>
+                </Link>
+              </Box>
+              <Box
+                sx={{
+                  position: "absolute",
+                  right: "50%",
+                  transform: "translateX(50%)",
+                }}
+              >
+                <Link href="/">
+                  <Image
+                    src="/images/logo-light-400w.png"
+                    width={100}
+                    alt="scripto logo"
+                  />
+                </Link>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Link
+                  href="/plans"
+                  style={{ textDecoration: "none", marginRight: "50px" }}
+                >
+                  <Text size={14} fw={700} color={colors.text}>
+                    Plans
+                  </Text>
+                </Link>
+                <Link
+                  href="/"
+                  style={{ textDecoration: "none", marginRight: "50px" }}
+                >
+                  <Text size={14} fw={700} color={colors.text}>
+                    Home
+                  </Text>
+                </Link>
+              </Box>
+            </Box>
+            <Divider size="sm" color={colors.text} />
+            <Button
+              mt={50}
+              h={60}
+              w={200}
+              radius={10}
+              sx={{
+                alignSelf: "center",
+                background: colors.button,
+                fontSize: 18,
+              }}
+            >
+              Get in Touch
+            </Button>
+            <Box
+              mt={50}
+              sx={{ display: "flex", alignSelf: "center", gap: "20px" }}
+            >
+              <Text>©️ 2010-{year}</Text>
+              <Text>Privacy-Terms</Text>
+            </Box>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      )}
+    </>
   );
 }
