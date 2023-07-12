6c3d133baa7e90dd5e498d3081a4c03aaba0e2bb
matthew-woodington <smwoodington@gmail.com>
2023-07-12 14:58:59 -0400

    remove placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -810,7 +810,6 @@ const LandingPage: PageWithLayout = () => {
                         alt="the break"
                         src="/images/show-logos/the-break-with-michelle-wolf.png"
                         width={"100%"}
-                        withPlaceholder
                       />
                     </Box>
                   </Box>
